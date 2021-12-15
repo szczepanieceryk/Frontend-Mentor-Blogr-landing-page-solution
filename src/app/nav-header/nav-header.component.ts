dea264c387189a2ee40f2ba317271513b008621a
Eryk Szczepaniec <szczepanieceryk@gmial.com>
2021-12-15 13:00:34 +0100

    add responsiveness styles

--- src/app/nav-header/nav-header.component.ts
+++ src/app/nav-header/nav-header.component.ts
@@ -6,13 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-header.component.scss']
 })
 export class NavHeaderComponent implements OnInit {
-
+  isActive = false;
+  dropdownIsActive = false;
   // content for header 
-  title:string = '  A modern publishing platform';
+  title:string = 'A modern publishing platform';
   text:string = 'Grow your audience and build your online brand';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  menuActive(){
+    this.isActive = !this.isActive
+  }
+
+  dropdownActive(){
+
+    this.dropdownIsActive = !this.dropdownIsActive;
+
+    console.log('działa')
+
   }
 }
